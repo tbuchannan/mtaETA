bc988dae425d2940d751fe031083b5ea99eb0a14
Terence Buchannan <terencebuchannan@gmail.com>
2017-10-15 01:03:27 -0400

    Correctly seperate North bound and South Bound trains

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const csv = require('csvtojson');
 const request = require('request');
 const tripsCSVFilePath = './csv/trips.csv';
 const stopsCSVFilePath = './csv/stops.csv';
+const stationsCSVFilePath = './csv/stations.csv';
 
 const mapDims = 0.006;
 const apiKey = '5478c04ea5da79c1c75aa912a1fb9fd9';
@@ -11,34 +12,35 @@ const apiKey = '5478c04ea5da79c1c75aa912a1fb9fd9';
 /* Feed Request Settings */
 let requestSettings = {
   method: 'GET',
-  url: `http://datamine.mta.info/mta_esi.php?key=${apiKey}&feed_id=2`,
   encoding: null
 };
 
 /* Train, Trip, Station Objects */
-let stationObject = {};
-let nearbyStations = {};
+let allStations = {};
 let tripsObject = {};
-let nearbyStationsETA = {};
 let stopsObject = {};
-let feedObject = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, A: 26, C: 26, E: 26,
-  N: 16, Q: 16, R: 16, W: 16, B: 21, D: 21, F: 21, M: 21, L: 2, G: 31};
-let feeds = "";
-let feedStr = "";
-let routes = {};
+
+let nearbyStations = {};
+let nearbyStationsETA = {};
+
+let feedObject = {
+  1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1,
+  A: 26, C: 26, E: 26,
+  N: 16, Q: 16, R: 16, W: 16,
+  B: 21, D: 21, F: 21, M: 21,
+  L: 2,
+  G: 31
+};
+let feedsToCall = {};
+
 /* Add Event Listeners */
-$('#locate_button').on('click', (e) => {
-  e.preventDefault();
-  $.get('http://ip-api.com/json',(data) => {
-      getNearbyStations(data);
-    });
-});
+
 
 /* Populate Train, Trip, Station Objects */
 csv()
-  .fromStream(request.get('http://web.mta.info/developers/data/nyct/subway/Stations.csv'))
+  .fromFile(stationsCSVFilePath)
   .on('json', (obj) => {
-  stationObject[obj["GTFS Stop ID"]] = obj;
+  allStations[obj["GTFS Stop ID"]] = obj;
 });
 
 csv()
@@ -53,34 +55,66 @@ csv()
   stopsObject[obj["stop_id"]] = obj;
 });
 
-/* Populate Nearby Trains Object */
+$('#locate_button').on('click', (e) => {
+  e.preventDefault();
+  $.get('http://ip-api.com/json',(data) => {
+      getNearbyStations(data);
+    });
+});
+
+/* Populate nearbyStations Object */
 const getNearbyStations = (data) => {
   // data.lat = 40.703811;
   // data.lon = -73.918425;
-  for(let id in stationObject){
+  for(let id in allStations){
     if (id){
-      let stationLat = parseFloat(stationObject[id]["GTFS Latitude"]);
-      let stationLong = parseFloat(stationObject[id]["GTFS Longitude"]);
+      let stationLat = parseFloat(allStations[id]["GTFS Latitude"]);
+      let stationLong = parseFloat(allStations[id]["GTFS Longitude"]);
       if ((Math.abs(stationLat - data.lat) <= mapDims) && (Math.abs(stationLong - data.lon) <= mapDims))  {
-        let stopId = stationObject[id]["GTFS Stop ID"];
-        nearbyStations[stopId] = stationObject[id];
-        nearbyStationsETA[stopId] = [];
+        let stopId = allStations[id]["GTFS Stop ID"];
+        nearbyStations[stopId] = allStations[id];
       }
     }
   }
-  getRoutes(nearbyStations);
-  getIncomingTrains(nearbyStations);
+  getRoutes();
 };
 
+
+
 /* Populate nearbyStationsETA */
-const getIncomingTrains = (stations) => {
+const getIncomingTrains = () => {
+  for (let feedId in feedsToCall) {
+    requestSettings['url'] = `http://datamine.mta.info/mta_esi.php?key=${apiKey}&feed_id=${feedId}`;
+    makeRequest();
+  }
+  console.log("IM DONE");
+};
+
+/* Populate Feeds Array */
+const getRoutes = () => {
+  let feedStr = "";
+  for (let key in nearbyStations) {
+    feedStr += nearbyStations[key]["Daytime Routes"] + " ";
+  }
+
+  for (let i = 0;  i < feedStr.length; i += 2) {
+    let routeLetter = feedStr[i];
+    let feedId = feedObject[routeLetter];
+
+    if (feedsToCall[feedId] === undefined){
+      feedsToCall[feedId] = feedId;
+    }
+  }
+  getIncomingTrains();
+};
+
+/* Call all feeds */
+const makeRequest = () => {
   let feed;
-
-
-
   request(requestSettings, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+
+      feed = (GtfsRealtimeBindings.FeedMessage.decode(body));
       // iterate over all of the trains
       feed.entity.forEach((train) => {
         // if it has scheduled stops
@@ -89,40 +123,25 @@ const getIncomingTrains = (stations) => {
           // iterate over all of a trains scheduled stops
           nextStops.forEach((stop, _ ,allStops) => {
             // stop_id includes N or S
-            let formattedStation = stop.stop_id.slice(0, -1);
+            let stationId = stop.stop_id;
+            let formattedStationID = stationId.slice(0, -1);
             // if upcoming stop is a nearbyStation push its ETA into ETA obj
             let destination = allStops[allStops.length - 1];
-            if(nearbyStations[formattedStation]){
-              populateNearByStation(formattedStation, stop, destination);
+            if(nearbyStations[formattedStationID]){
+              populateNearByStation(stationId, stop, destination);
             }
           });
         }
       });
     }
+    display();
   });
+};
+
+const display = () => {
 
 
 };
-
-/* populate Feeds Array */
-const getRoutes = (stations) => {
-  feedStr = "";
-  for (let key in stations) {
-    feedStr += stations[key]["Daytime Routes"] + " ";
-  }
-
-  feedStr = feedStr.split(" ");
-  feedStr.pop();
-
-  for (let i = 0;  i < feedStr.length; i++) {
-    let routeLetter = feedStr[i];
-    let route = feedObject[routeLetter];
-    routes[route] = route;
-  }
-
-};
-
-
 /* Compare station times */
 
 const populateNearByStation = (station, stop, destination) => {
@@ -131,6 +150,10 @@ const populateNearByStation = (station, stop, destination) => {
     destination: stopsObject[destination.stop_id].stop_name,
     stop_name: station
   };
+
+  if(nearbyStationsETA[station] === undefined){
+    nearbyStationsETA[station] = [];
+  }
 
   let first = nearbyStationsETA[station][0];
   let second = nearbyStationsETA[station][1];
