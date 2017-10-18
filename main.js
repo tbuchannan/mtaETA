681843e664c5d2fbf2692a6f6628a606efb78820
Terence Buchannan <terencebuchannan@gmail.com>
2017-10-18 19:51:58 -0400

    Add default app background color

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const url = require('url');
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 1200, height: 800});
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: '#DED9E2'
+  });
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
