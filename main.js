9a6657a9ded76f1339a10179082ac33c497ddbbd
Terence Buchannan <terencebuchannan@gmail.com>
2017-10-28 00:42:28 -0400

    Change styling

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 1200,
     height: 800,
-    backgroundColor: '#DED9E2'
+    backgroundColor: '#FFFAFA'
   });
 
   win.loadURL(url.format({
